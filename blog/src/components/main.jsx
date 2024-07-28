b189950d74b4040a4ba8a92fad674aa10890073c
Kav <kevincamilo56@gmail.com>
2024-07-28 18:06:17 -0500

    Actualizo diseño de la pagina principal

--- blog/src/components/main.jsx
+++ blog/src/components/main.jsx
@@ -14,51 +14,6 @@ const Main = () => {
                         </div>
                     </div>
                 </div>
-                <div className="contenedor-card">
-                    <div class="card">
-                        <img src={logo} class="card-img-top" alt={logo} />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="contenedor-card">
-                    <div class="card">
-                        <img src={logo} class="card-img-top" alt={logo} />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="contenedor-card">
-                    <div class="card">
-                        <img src={logo} class="card-img-top" alt={logo} />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="contenedor-card">
-                    <div class="card">
-                        <img src={logo} class="card-img-top" alt={logo} />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="contenedor-card">
-                    <div class="card">
-                        <img src={logo} class="card-img-top" alt={logo} />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     )
